64372c3a02e296151d4ed7329e178cd57a5634f2
Bernhard Rosenkränzer <bero@lindev.ch>
2019-06-29 21:41:22 +0200

    Move languages to lang/ directory

---
+++ lang/en.ts
@@ -0,0 +1,93 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>MainDialog</name>
+    <message>
+        <location filename="../src/MainDialog.cpp" line="10"/>
+        <source>OpenMandriva Software Repository Picker</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainDialog.cpp" line="20"/>
+        <source>Below, you can select from which repositories you want to install and update packages (extra applications, games, etc.).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainDialog.cpp" line="22"/>
+        <source>32-bit repositories contain the same packages built for 32 bit processors. Those repositories are usually useful only for compatibility with prebuilt packages for older systems - such as binary-only games and applications built for 32-bit processors and running 32-bit Windows applications in Wine.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainDialog.cpp" line="36"/>
+        <source>&amp;OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainDialog.cpp" line="38"/>
+        <source>&amp;Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainDialog.cpp" line="170"/>
+        <source>Update channel changed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainDialog.cpp" line="170"/>
+        <source>The update channel has been changed. You probably want to refresh all packages in the graphical package manager or by running &quot;dnf --refresh distro-sync&quot; in a command line.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RepoWidget</name>
+    <message>
+        <location filename="../src/RepoWidget.cpp" line="10"/>
+        <source>&amp;Enable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/RepoWidget.cpp" line="19"/>
+        <source>Ena&amp;ble 32-bit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UpdateChannelPicker</name>
+    <message>
+        <location filename="../src/UpdateChannelPicker.cpp" line="7"/>
+        <source>U&amp;pdate channel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/UpdateChannelPicker.cpp" line="9"/>
+        <source>&amp;Update channel:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/UpdateChannelPicker.cpp" line="19"/>
+        <source>Enable &amp;testing repositories (Updates ahead of time for QA)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/UpdateChannelPicker.cpp" line="30"/>
+        <source>Really try to downgrade?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/UpdateChannelPicker.cpp" line="30"/>
+        <source>You&apos;re trying to switch to an update channel older than what you&apos;re currently using. This will result in downgrading packages, which may or may not work. Going back to older versions is unsupported (older versions of applications can&apos;t know about potential changes in newer versions...) and may well break your system. Are you sure you want to do this?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/UpdateChannelPicker.cpp" line="35"/>
+        <source>Really upgrade?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/UpdateChannelPicker.cpp" line="35"/>
+        <source>You&apos;re about to switch to a faster update channel. Please note that this is a permanent switch, downgrading your system by going back to a slower update channel is not supported and may break your system. Are you sure you want to do this?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
